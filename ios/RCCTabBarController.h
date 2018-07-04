796e34f2cdf7187566ad1792967aa41f4b4c2892
Nathaniel Ram <nathaniel.ram@osedea.com>
2018-07-04 15:58:44 -0400

    resize overlay view on iOS on rotation

--- ios/RCCTabBarController.h
+++ ios/RCCTabBarController.h
@@ -5,7 +5,9 @@
 
 - (instancetype)initWithProps:(NSDictionary *)props children:(NSArray *)children globalProps:(NSDictionary*)globalProps bridge:(RCTBridge *)bridge;
 - (void)performAction:(NSString*)performAction actionParams:(NSDictionary*)actionParams bridge:(RCTBridge *)bridge completion:(void (^)(void))completion;
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator;
 
 @property (nonatomic) BOOL tabBarHidden;
+@property (nonatomic) UIView *overlayView;
 
 @end
